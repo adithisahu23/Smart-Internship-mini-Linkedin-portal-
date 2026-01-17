ace9f5d3cef60ca31446735f8d11a91fff86c312
ADITI SAHU <0105cd231018@oriental.ac.in>
2026-01-17 16:10:49 +0530

    Create App component with routing for pages

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Jobs from "./pages/Jobs";
+
+export default function App() {
+  return (
+    <>
+      <Register />
+      <Login />
+      <Jobs />
+    </>
+  );
+}
